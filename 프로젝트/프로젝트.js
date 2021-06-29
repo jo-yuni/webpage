fed21a4ad2989818e7ed3a3386a347fc81600264
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2021-06-29 11:18:59 +0900

    여러가지근데이제 홍진경의저주를곁들인

--- 프로젝트/프로젝트.js
+++ 프로젝트/프로젝트.js
@@ -1,32 +1,32 @@
 document.getElementById('content1').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('content2').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('content3').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('content4').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('content5').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('content6').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('content7').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.add('showanimation')
-    document.getElementsByClassName('content')[0].classList.add('hidden')
+    // document.getElementsByClassName('content')[0].classList.add('hidden')
 })
 document.getElementById('arrow').addEventListener('click', function() {
     document.getElementsByClassName('add')[0].classList.remove('showanimation')
-    document.getElementsByClassName('content')[0].classList.remove('hidden')
+    // document.getElementsByClassName('content')[0].classList.remove('hidden')
 })
